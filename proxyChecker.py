4481a31daa0975c1eeefa31f34381efa5a28f6b9
unnecessary-axiom <davis@admin2.oic-hq.com>
2016-08-24 13:00:51 -0700

    Remove force 1 connection

--- proxyChecker.py
+++ proxyChecker.py
@@ -30,13 +30,6 @@ def isGoodProxy(target_address, proxy_string, timeout=8, canary_text=None):
         'https' : proxy_string, 
     }
 
-    s = requests.Session()
-    # Kind of a hacky/nosy inspection to see if I need to apply my retry limit
-    if s.adapters['http://'].max_retries != 1 or s.adapters['https://'].max_retries != 1:
-        a = requests.adapters.HTTPAdapter(max_retries=1)
-        s.mount('http://', a)
-        s.mount('https://', a)
-
     try: 
         response = requests.get(target_address, timeout=timeout, proxies=proxies)
     except (requests.exceptions.Timeout,requests.exceptions.ConnectionError) as e:
